0cfc1c58c04f01112f3d0a84e3aff804d6624de9
Dennis Garcia <ddgarcia@theimaginegroup.com>
2024-03-05 10:08:39 -0600

    This is a test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
-import Navbar from './components/navbar/navbar';
-import styles from './page.module.css';
-import React from 'react';
+import Navbar from "./components/navbar/navbar";
+import styles from "./page.module.css";
+import React from "react";
 
 export default function Home() {
   const labelMap = {
-    lblHome: 'Dallas Software Developers',
-    lblCommunity: 'Community Impact',
-    lblContact: 'Contact Us',
-    lblMeetup: 'Meetups',
+    lblHome: "Dallas Software Developers",
+    lblCommunity: "Community Impact",
+    lblContact: "Contact Us",
+    lblMeetup: "Meetups",
   };
 
+  // Render the page
   return (
     <main className={styles.main}>
       <Navbar label={labelMap} />
