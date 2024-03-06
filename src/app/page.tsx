fa757a45e16cbbe37b609ae14dff5c74d02a706a
Joshua Ow <joshuaow@gmail.com>
2024-03-06 11:41:42 -0600

    chore: Moved card data to the home component and passed as props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,9 @@ export default function Home() {
   };
 
   const cardData = [
-    { id: 1, content: labelMap.lblHome },
-    { id: 2, content: labelMap.lblCommunity },
-    { id: 3, content: labelMap.lblContact },
-    { id: 4, content: labelMap.lblMeetup },
+    { id: 1, content: "Technical Workshops" },
+    { id: 2, content: "Community Support" },
+    { id: 3, content: "Cohorts & Hackathons" },
   ];
 
   return (
