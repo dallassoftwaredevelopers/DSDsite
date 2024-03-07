e88ec435622508d16a38ac6773a11d6348bd89ab
Ryan Monaghan <ryan@monaghan.xyz>
2024-03-07 10:29:34 -0600

    Addes content section for the cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,15 @@ export default function Home() {
     lblContact: "Contact Us",
     lblMeetup: "Meetups",
     lblBanner: "This website is made BY the community FOR the community",
-    lblWorkshops: "Technical Workshops",
-    lblSupport: "Community Support",
-    lblCohorts: "Cohorts & Hackathons",
+    lblWorkshopsTitle: "Technical Workshops",
+    lblSupportTitle: "Community Support",
+    lblCohortsTitle: "Cohorts & Hackathons",
+    lblWorkshopsContent:
+      "Bringing the community together twice a month around technical topics that allow you to learn and grow your programming skills to be a stronger developer.",
+    lblSupportContent:
+      "No matter your level, you have a community that has your back! Dallas Software Developers are focused on supporting our local community while also trying to support the developers who need our help and support!",
+    lblCohortsContent:
+      "A 6-week program that is completely free to pair developers working on a project being guided by a developer working in the industry. The focus is to help give you something interesting to showcase in an interview and give you real-world skills!",
   };
 
   return (
