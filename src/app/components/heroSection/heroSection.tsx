e6ef21be314854c4d5bcb631c9f203cc9faf7a68
Danny <dannythompson901@gmail.com>
2024-03-23 23:39:34 -0500

    chore: mobile hero section adjustment with image (WEB-25)

--- src/app/components/heroSection/heroSection.tsx
+++ src/app/components/heroSection/heroSection.tsx
@@ -2,12 +2,21 @@
 
 import React from 'react';
 import styles from './heroSection.module.css';
+import Image from 'next/image';
 
 export default function HeroSection({ label }: { label: string }) {
   return (
     <div className={styles.heroSection} data-testid='hero'>
       <p className={styles.intro}>{label}</p>
       <div className={styles.videoPlaceholder}>Video playing here</div>
+      <div className={styles.mobileHeroImage}>
+        <Image
+          src='/assets/mobileHeroImage.png'
+          alt='Hero image'
+          height={200}
+          width={200}
+        />
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import HeroSection from './components/heroSection/heroSection';
 import Navbar from './components/navbar/navbar';
 import styles from './page.module.css';
 
+
 export default function Home() {
   const labelMap = {
     lblHome: 'Dallas Software Developers',
