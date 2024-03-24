0e50ccee0aba69623e112a088155258d445d816c
Danny <dannythompson901@gmail.com>
2024-03-23 23:42:01 -0500

    prettier fixes on main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import HeroSection from './components/heroSection/heroSection';
 import Navbar from './components/navbar/navbar';
 import styles from './page.module.css';
 
-
 export default function Home() {
   const labelMap = {
     lblHome: 'Dallas Software Developers',
