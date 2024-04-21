d2497fc5a846b9f95419549911569f387e717f91
Claire Carden <smclairecarden@gmail.com>
2024-04-20 20:19:27 -0500

    #73_groupPhoto prettier?

--- src/app/components/groupPhotoSection/groupPhotoSection.tsx
+++ src/app/components/groupPhotoSection/groupPhotoSection.tsx
@@ -8,13 +8,13 @@ import { detectContentType } from 'next/dist/server/image-optimizer';
 export default function GroupPhotoSection({ label }: { label: string }) {
   return (
     <div className={styles.groupPhotoSection} data-testid='groupPhoto'>
-        <Image
-          className={styles.groupPhotoImg}
-          src='/assets/meetupGroupShot5.png'
-          alt='Meetup Group Shot image'
-          width={1450}
-          height={500}
-        />
+      <Image
+        className={styles.groupPhotoImg}
+        src='/assets/meetupGroupShot5.png'
+        alt='Meetup Group Shot image'
+        width={1450}
+        height={500}
+      />
     </div>
   );
 }
