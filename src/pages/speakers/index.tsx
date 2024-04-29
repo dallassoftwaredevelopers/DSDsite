7cf2d46beb5ad008b310c4ac0aaa0e19bf64eaaa
Soumava-221B <77767745+Soumava-221B@users.noreply.github.com>
2024-04-29 23:31:44 +0000

    chore: create a new page directory for speakers (WEB-41) #87

---
+++ src/pages/speakers/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './pastSpeakers.module.css';
+
+const Speakers: React.FC = () => {
+  return (
+    <div>
+      <h1 className={styles.h1}>Past Speakers</h1>
+    </div>
+  );
+};
+
+export default Speakers;
