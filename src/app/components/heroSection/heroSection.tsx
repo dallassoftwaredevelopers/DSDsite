69f7ac42d8ab320f0589041d8a1f90a24b26d77b
Danny <dannythompson901@gmail.com>
2024-08-25 00:55:10 -0500

    (WEB-32) chore: apply new design changes on herosection.

--- src/app/components/heroSection/heroSection.tsx
+++ src/app/components/heroSection/heroSection.tsx
@@ -6,16 +6,14 @@ import Image from 'next/image';
 
 export default function HeroSection() {
   return (
-    <div className={styles.heroSection} data-testid='hero'>
-      <p className={styles.intro}>{'Level Up Your Coding with DSD'}</p>
-      <div className={styles.videoPlaceholder}>Video playing here</div>
-      <div className={styles.mobileHeroImage}>
-        <Image
-          src='/assets/mobileHeroImage.png'
-          alt='Hero image'
-          height={200}
-          width={200}
-        />
+    <div className={styles.hero}>
+      <div className={styles.purpleShape}></div>
+      <div className={styles.heroSection} data-testid='hero'>
+        <p className={styles.intro}>{'Level Up Your Coding with DSD'}</p>
+      </div>
+
+      <div>
+        <div className={styles.videoPlaceholder}>Video playing here</div>
       </div>
     </div>
   );
