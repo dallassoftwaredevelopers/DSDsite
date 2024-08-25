f80a2953283e53deee3dbe7c41544715afe165c1
Danny <dannythompson901@gmail.com>
2024-08-25 11:56:12 -0500

    (WEB-43) chore: adjust hero section to new design

--- src/app/components/heroSection/heroSection.tsx
+++ src/app/components/heroSection/heroSection.tsx
@@ -2,19 +2,16 @@
 
 import React from 'react';
 import styles from './heroSection.module.css';
-import Image from 'next/image';
 
 export default function HeroSection() {
   return (
-    <div className={styles.hero}>
-      <div className={styles.purpleShape}></div>
-      <div className={styles.heroSection} data-testid='hero'>
-        <p className={styles.intro}>{'Level Up Your Coding with DSD'}</p>
+    <section className={styles.hero}>
+      <div className={styles.heroSection}>
+        <h1 className={styles.intro}>Level Up Your Coding with DSD</h1>
       </div>
-
-      <div>
-        <div className={styles.videoPlaceholder}>Video playing here</div>
+      <div className={styles.purpleShape}>
+        <div className={styles.videoPlaceholder}>Video Playing Here</div>
       </div>
-    </div>
+    </section>
   );
 }
