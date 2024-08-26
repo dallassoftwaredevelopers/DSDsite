578e46da7e72150e315c780a0f1831706ddbd310
Yoojin93 <yoojinbae93@gmail.com>
2024-08-25 22:23:36 -0500

    (WEB-47) chore: run prettier

--- src/app/about/aboutData.tsx
+++ src/app/about/aboutData.tsx
@@ -1,22 +1,22 @@
 export interface AboutSection {
-    id: string;
-    title: string;
-    description: string;
-    imageSrc?: string;
+  id: string;
+  title: string;
+  description: string;
+  imageSrc?: string;
 }
 
 export const descriptions = {
-    aboutDescription: `
+  aboutDescription: `
     Explore what Dallas Software Developers as to offer and become an integral part of our dynamic community. Whether you're passionate about coding, eager to contribute to open-source projects, or simply looking to connect with like-minded individuals, there's a place for you here.
 
     Dive into our various programs, from collaborative projects on Discord to hands-on developer placement initiatives, and discover how you can make a meaningful impact while honing your skills and expanding your network.
     `,
-    opensourceDescription: `
+  opensourceDescription: `
     Join the dynamic community of [organization] on Discord, where you'll find an array of collaborative projects aimed at enhancing teamwork, learning opportunities, and hands-on contributions to open-source code. Our Discord serves as the central hub for developers eager to engage in meaningful discussions, seek advice from peers, and actively participate in real-world coding projects.
 
     On our Discord there are a selection of collaborative projects, each offering a unique chance to learn and contribute. With project codebases hosted on GitHub, you'll have easy access to the tools and resources necessary to get started. Simply head over to [x] channel to explore current projects and find one that aligns with your interests and expertise. Whether you're passionate about web development, mobile apps, or AI, there's a project waiting for you to make your mark. Join us on Discord and be part of a vibrant community dedicated to fostering collaboration and growth in the world of coding.
     `,
-  };
+};
 
 export const aboutData: AboutSection[] = [
   {
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import { aboutData } from './aboutData';
-import styles from './community.module.css';
+import styles from './about.module.css';
 
 export default function AboutPage() {
   return (
     <>
-    <section className={styles.aboutContainer}>
-      {/* About Section */}
-      <div className={styles.aboutSection}>
-        <h1 className={styles.aboutTitle}>{aboutData[0].title}</h1>
-        <div className={styles.imageSection}>
-          <img
-            src='/assets/communitySupport.png'
-            className={styles.mainImage}
-          />
+      <section className={styles.aboutContainer}>
+        {/* About Section */}
+        <div className={styles.aboutSection}>
+          <h1 className={styles.aboutTitle}>{aboutData[0].title}</h1>
+          <div className={styles.imageSection}>
+            <img
+              src='/assets/communitySupport.png'
+              className={styles.mainImage}
+            />
+          </div>
+          <p className={styles.aboutDescription}>{aboutData[0].description}</p>
         </div>
-        <p className={styles.aboutDescription}>{aboutData[0].description}</p>
-      </div>
-    </section>
-
+      </section>
       {/* Blue Container wrapping the Open Source Projects Section */}
       <section className={styles.blueContainer}>
         <div className={styles.opensourceSection}>
           <h2 className={styles.opensourceTitle}>{aboutData[1].title}</h2>
-          <p className={styles.opensourceDescription}>{aboutData[1].description}</p>
+          <p className={styles.opensourceDescription}>
+            {aboutData[1].description}
+          </p>
         </div>
       </section>
     </>
