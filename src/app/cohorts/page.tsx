751f05409b726d73aa15f99231f20f0e26a0527a
Diss <65290988+Dissurender@users.noreply.github.com>
2024-08-26 12:08:56 -0500

    move and format cohort list to horizontal orientation

--- src/app/cohorts/page.tsx
+++ src/app/cohorts/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import React from 'react';
 import styles from './cohorts.module.css';
 import OfferingCard from '../components/offeringCard/offeringCard';
@@ -8,75 +8,128 @@ export default function CohortPage() {
   const [selectedYear, setSelectedYear] = React.useState<number>(2024);
 
   const cohortsText = [
-    "Embark on a six-week journey with our completely free developer placement program, designed to pair developers like you with industry professionals for hands-on project experience. Guided by seasoned developers, this program focuses on equipping you with real-world skills and creating compelling projects to showcase in interviews.",
+    'Embark on a six-week journey with our completely free developer placement program, designed to pair developers like you with industry professionals for hands-on project experience. Guided by seasoned developers, this program focuses on equipping you with real-world skills and creating compelling projects to showcase in interviews.',
     "Through our developer placement program, you'll collaborate closely with a team on a project, gaining invaluable insights and mentorship from professionals actively working in the industry. This immersive experience is crafted to empower you with practical skills and knowledge, enhancing your readiness for the job market and elevating your confidence in interviews.",
     "Join us for a dynamic program where learning meets application, and where the projects you work on aren't just exercises but tangible demonstrations of your abilities. Don't miss this opportunity to turbocharge your career with practical experience, industry guidance, and a portfolio of impressive projects that truly set you apart.",
   ];
 
-  interface Cohort {
+  interface Group {
     id: number;
-    cohortName: string;
+    groupName: string;
     youtubeLink: string;
+    githubLink: string;
     imageUrl: string;
   }
 
   type CohortData = {
-    [year: number]: Cohort[];
+    [year: number]: Group[];
   };
 
+  // Data for the cohorts, add more groups to each year as needed
   const cohortData: CohortData = {
     2022: [
       {
         id: 1,
-        cohortName: 'Spring',
+        groupName: 'Group 1',
         youtubeLink: '/',
+        githubLink: '/',
         imageUrl: '/assets/person.svg',
       },
       {
         id: 2,
-        cohortName: 'Fall',
+        groupName: 'Group 2',
         youtubeLink: '/',
+        githubLink: '/',
+        imageUrl: '/assets/person.svg',
+      },
+
+      {
+        id: 3,
+        groupName: 'Group 3',
+        youtubeLink: '/',
+        githubLink: '/',
+        imageUrl: '/assets/person.svg',
+      },
+      {
+        id: 4,
+        groupName: 'Group 4',
+        youtubeLink: '/',
+        githubLink: '/',
         imageUrl: '/assets/person.svg',
       },
     ],
     2023: [
       {
         id: 1,
-        cohortName: 'Spring',
+        groupName: 'Group 1',
         youtubeLink: '/',
+        githubLink: '/',
         imageUrl: '/assets/person.svg',
       },
       {
         id: 2,
-        cohortName: 'Fall',
+        groupName: 'Group 2',
         youtubeLink: '/',
+        githubLink: '/',
+        imageUrl: '/assets/person.svg',
+      },
+      {
+        id: 3,
+        groupName: 'Group 3',
+        youtubeLink: '/',
+        githubLink: '/',
+        imageUrl: '/assets/person.svg',
+      },
+      {
+        id: 4,
+        groupName: 'Group 4',
+        youtubeLink: '/',
+        githubLink: '/',
         imageUrl: '/assets/person.svg',
       },
     ],
     2024: [
       {
         id: 1,
-        cohortName: 'Spring',
+        groupName: 'Group 1',
         youtubeLink: '/',
+        githubLink: '/',
         imageUrl: '/assets/person.svg',
       },
       {
         id: 2,
-        cohortName: 'Fall',
+        groupName: 'Group 2',
         youtubeLink: '/',
+        githubLink: '/',
+        imageUrl: '/assets/person.svg',
+      },
+
+      {
+        id: 3,
+        groupName: 'Group 3',
+        youtubeLink: '/',
+        githubLink: '/',
+        imageUrl: '/assets/person.svg',
+      },
+      {
+        id: 4,
+        groupName: 'Group 4',
+        youtubeLink: '/',
+        githubLink: '/',
         imageUrl: '/assets/person.svg',
       },
     ],
-  }; 
+  };
 
   return (
     <section className={styles.container}>
       <div className={styles.titleSection}>
         <h1 className={styles.title}>Cohorts</h1>
+
         <div className={styles.paragraphBox}>
-        <p className={styles.cohortsParagraph}>{cohortsText[0]}</p>
-        <p className={styles.cohortsParagraph}>{cohortsText[1]}</p>
-        <p className={styles.cohortsParagraph}>{cohortsText[2]}</p>
+          <p className={styles.cohortsParagraph}>{cohortsText[0]}</p>
+          <p className={styles.cohortsParagraph}>{cohortsText[1]}</p>
+          <p className={styles.cohortsParagraph}>{cohortsText[2]}</p>
         </div>
 
         <h2 className={styles.subtitle}>Cohort Status</h2>
@@ -96,23 +149,25 @@ export default function CohortPage() {
                 key={year}
                 className={`${styles.yearButton} ${
                   selectedYear === Number(year) ? styles.selectedYear : ''
-                  }`}
+                }`}
                 onClick={() => setSelectedYear(Number(year))}
               >
                 {year}
               </button>
             ))}
         </div>
+
         <div className={styles.cohortList}>
           {cohortData[selectedYear].map((cohort) => (
-              <CohortCard 
-                key={cohort.id}
-                cohortName={cohort.cohortName}
-                youtubeLink={cohort.youtubeLink}
-                imageUrl={cohort.imageUrl}
-              />
+            <CohortCard
+              key={cohort.id}
+              groupName={cohort.groupName}
+              youtubeLink='/'
+              githubLink='/'
+              imageUrl='/assets/person.svg'
+            />
           ))}
-          </div>
+        </div>
       </div>
     </section>
   );
--- src/app/components/cohortCard/cohortCard.tsx
+++ src/app/components/cohortCard/cohortCard.tsx
@@ -2,30 +2,58 @@ import Image from 'next/image';
 import styles from './cohortCard.module.css';
 
 interface CohortCardProps {
-  cohortName: string;
+  groupName: string;
   youtubeLink?: string;
+  githubLink?: string;
   imageUrl?: string;
 }
 
 export default function CohortCard({
-  cohortName,
+  groupName,
   youtubeLink,
   imageUrl,
+  githubLink,
 }: CohortCardProps) {
+  const Thumbnail = imageUrl ? (
+    <div className={styles.cohortImageContainer}>
+      <Image
+        className={styles.cohortImage}
+        width={200}
+        height={200}
+        src={imageUrl}
+        alt='Thumbnail of cohort video'
+      />
+    </div>
+  ) : (
+    <div className={styles.defaultImageContainer}>
+      <Image
+        className={styles.defaultImage}
+        width={200}
+        height={200}
+        src='/assets/person.svg'
+        alt='Default thumbnail for cohort video'
+      />
+    </div>
+  );
+
   return (
-    <div className={styles.cohortCard}>
-      <div className={styles.cohortImageContainer}>
-        <Image
-          className={styles.cohortImage}
-          width={200}
-          height={200}
-          src={imageUrl}
-          alt='image of person'
-        />
-      </div>
+    <div className={styles.cohortContainer}>
+      <a href={youtubeLink}>{Thumbnail}</a>
       <div className={styles.cohortInfo}>
-        <h3 className={styles.cohortName}>{cohortName}</h3>
-        <a href={youtubeLink}>Watch on YouTube</a>
+        <h3 className={styles.cohortName}>
+          {groupName}
+          {githubLink && (
+            <a href={githubLink} className={styles.ghLink}>
+              <Image
+                className={styles.icon}
+                width={30}
+                height={30}
+                src='/assets/githubIcon.png'
+                alt='Github social icon'
+              />
+            </a>
+          )}
+        </h3>
       </div>
     </div>
   );
