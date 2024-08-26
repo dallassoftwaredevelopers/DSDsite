57a4c8d06b952a7102aa75c42b574032d4a4687b
Yoojin93 <yoojinbae93@gmail.com>
2024-08-25 22:13:45 -0500

    (WEB-47) chore: add about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import { aboutData } from './aboutData';
+import styles from './community.module.css';
 
 export default function AboutPage() {
-  return <div>About page</div>;
+  return (
+    <>
+    <section className={styles.aboutContainer}>
+      {/* About Section */}
+      <div className={styles.aboutSection}>
+        <h1 className={styles.aboutTitle}>{aboutData[0].title}</h1>
+        <div className={styles.imageSection}>
+          <img
+            src='/assets/communitySupport.png'
+            className={styles.mainImage}
+          />
+        </div>
+        <p className={styles.aboutDescription}>{aboutData[0].description}</p>
+      </div>
+    </section>
+
+      {/* Blue Container wrapping the Open Source Projects Section */}
+      <section className={styles.blueContainer}>
+        <div className={styles.opensourceSection}>
+          <h2 className={styles.opensourceTitle}>{aboutData[1].title}</h2>
+          <p className={styles.opensourceDescription}>{aboutData[1].description}</p>
+        </div>
+      </section>
+    </>
+  );
 }
