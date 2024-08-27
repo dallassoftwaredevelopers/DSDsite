5b6b1713fc0ff5fec454f38102fcfb04bb533eb0
Dennis Garcia <BlueBoxDev@outlook.com>
2024-08-26 21:14:33 -0500

    (WEB-52) CHORE: Fix styles on About page to make it mobile responsive

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { aboutData } from './aboutData';
+import Image from 'next/image';
 import styles from './about.module.css';
 
 export default function AboutPage() {
@@ -7,25 +7,54 @@ export default function AboutPage() {
     <>
       {/* About Section */}
       <section className={styles.aboutContainer}>
-        <div className={styles.aboutSection}>
-          <h1 className={styles.aboutTitle}>{aboutData[0].title}</h1>
-          <div className={styles.imageSection}>
-            <img
-              src='/assets/communitySupport.png'
-              className={styles.mainImage}
-            />
-          </div>
-          <p className={styles.aboutDescription}>{aboutData[0].description}</p>
+        <h1 className={styles.aboutTitle}>Open Source Projects</h1>
+        <div className={styles.imageSection}>
+          <Image
+            src='/assets/communitySupport.png'
+            alt='community photo of solving a problem'
+            fill
+            objectFit='contain'
+          />
         </div>
+        <p className={styles.aboutDescription}>
+          Explore what Dallas Software Developers as to offer and become an
+          integral part of our dynamic community. Whether you&apos;re passionate
+          about coding, eager to contribute to open-source projects, or simply
+          looking to connect with like-minded individuals, there&apos;s a place
+          for you here.
+        </p>
+        <p className={styles.aboutDescription}>
+          Dive into our various programs, from collaborative projects on Discord
+          to hands-on developer placement initiatives, and discover how you can
+          make a meaningful impact while honing your skills and expanding your
+          network.
+        </p>
       </section>
       {/* Open Source Projects Section */}
       <section className={styles.opensourceContainer}>
-        <div className={styles.opensourceSection}>
-          <h2 className={styles.opensourceTitle}>{aboutData[1].title}</h2>
-          <p className={styles.opensourceDescription}>
-            {aboutData[1].description}
-          </p>
-        </div>
+        <h2 className={styles.opensourceTitle}>
+          About Dallas Software Developers
+        </h2>
+        <p className={styles.opensourceDescription}>
+          Join the dynamic community of [organization] on Discord, where
+          you&apos;ll find an array of collaborative projects aimed at enhancing
+          teamwork, learning opportunities, and hands-on contributions to
+          open-source code. Our Discord serves as the central hub for developers
+          eager to engage in meaningful discussions, seek advice from peers, and
+          actively participate in real-world coding projects.
+        </p>
+        <p className={styles.opensourceDescription}>
+          On our Discord there are a selection of collaborative projects, each
+          offering a unique chance to learn and contribute. With project
+          codebases hosted on GitHub, you&apos;ll have easy access to the tools
+          and resources necessary to get started. Simply head over to [x]
+          channel to explore current projects and find one that aligns with your
+          interests and expertise. Whether you&apos;re passionate about web
+          development, mobile apps, or AI, there&apos;s a project waiting for
+          you to make your mark. Join us on Discord and be part of a vibrant
+          community dedicated to fostering collaboration and growth in the world
+          of coding.
+        </p>
       </section>
     </>
   );
