5d603f3afdf1279ab37f00f2d41072dcf9f2a273
Diss <65290988+Dissurender@users.noreply.github.com>
2024-08-27 08:41:34 -0500

    prettier run

--- src/app/components/offeringCard/offeringCard.tsx
+++ src/app/components/offeringCard/offeringCard.tsx
@@ -1,6 +1,6 @@
 import styles from './offeringCard.module.css';
 
-const defaultText = "Want to become a speaker at an event?"
+const defaultText = 'Want to become a speaker at an event?';
 
 interface OfferingCardProps {
   text?: string;
@@ -16,9 +16,7 @@ export default function OfferingCard({
   return (
     <div className={styles.offeringCard}>
       <div className={styles.textContainer}>
-        <p className={styles.text}>
-          {text || defaultText}
-        </p>
+        <p className={styles.text}>{text || defaultText}</p>
       </div>
       <a href={buttonLink} className={styles.button}>
         {buttonText}
