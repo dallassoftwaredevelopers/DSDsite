5dfe4da8d9381156b108bce2b95c5ca6e8ca3d5c
Diss <65290988+Dissurender@users.noreply.github.com>
2024-08-28 08:11:13 -0500

    video component responsive

--- src/app/components/video/videoFrame.tsx
+++ src/app/components/video/videoFrame.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import useMediaQuery from '@/app/hooks/useMediaQuery';
+import React, { use } from 'react';
 
 interface VideoProps {
   videoUrl: string;
@@ -7,11 +8,24 @@ interface VideoProps {
 
 // Video component to display a video iframe - using Youtube embed specifications
 export default function Video({ videoUrl, title }: VideoProps) {
+
+  let width = 560;
+  let height = 315;
+
+  if (useMediaQuery('(max-width: 1050px)')) {
+    width = 480;
+    height = 270;
+  }
+  if (useMediaQuery('(max-width: 850px)')) {
+    width = 320;
+    height = 180;
+  }
+
   return (
     <div className='videoFrame'>
       <iframe
-        width='560'
-        height='315'
+        width={width}
+        height={height}
         src={videoUrl}
         title={title}
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
