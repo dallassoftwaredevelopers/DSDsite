bc7a721b1c8f0b6b56c9ca5dc5ccf2b6d5d399e1
Dennis Garcia <BlueBoxDev@outlook.com>
2024-09-01 17:22:05 -0500

    (WEB-77) update verbiage

--- src/app/components/bannerSection/bannerSection.tsx
+++ src/app/components/bannerSection/bannerSection.tsx
@@ -19,15 +19,15 @@ export default function BannerSection() {
         <h1>WHO WE ARE</h1>
         <p>
           Dallas Software Developers group is a local community in the DFW area
-          that is ran 100% by volunteers, with our primary focus being to bring
-          as much value, resources and to build a thriving community to the
-          local software developer market.
+          that is run 100% by volunteers, with our primary focus being to bring
+          as much value and resources as possible and build a thriving community
+          to the local software developer market.
         </p>
         <p>
           Level up your coding by participating in our quarterly cohorts or gain
-          valuable insight on various coding topics such as how to optimize your
-          code in JavaScript, object oriented programming, Machine Learning and
-          AI, and many more.
+          valuable insight on various coding topics, such as optimizing your
+          code in JavaScript, learning object-oriented programming, using
+          machine learning, leveraging AI&apos;s power, and much more.
         </p>
       </div>
     </div>
