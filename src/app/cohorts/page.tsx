15e936e200e6501c68dca336f3bf123ad8c712d2
Dennis Garcia <BlueBoxDev@outlook.com>
2024-09-01 17:18:56 -0500

    (WEB-73) Use "Section" component and slim down intro description

--- src/app/cohorts/page.tsx
+++ src/app/cohorts/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import styles from './cohorts.module.css';
 import OfferingCard from '../components/offeringCard/offeringCard';
 import CohortCard from '../components/cohortCard/cohortCard';
+import Section from '../components/Section/section';
 
 // TODO: Add appropriate links
 
@@ -65,77 +66,56 @@ export default function CohortPage() {
 
   return (
     <>
-      <section className='sectionIntro bgBlue'>
-        <div className='sectionPadded'>
-          <h1>Cohorts</h1>
-          <p className='sectionParagraph'>
-            Embark on a six-week journey with our completely free developer
-            placement program, designed to pair developers like you with
-            industry professionals for hands-on project experience. Guided by
-            seasoned developers, this program focuses on equipping you with
-            real-world skills and creating compelling projects to showcase in
-            interviews.
-          </p>
-          <p className='sectionParagraph'>
-            Through our developer placement program, you&apos;ll collaborate
-            closely with a team on a project, gaining invaluable insights and
-            mentorship from professionals actively working in the industry. This
-            immersive experience is crafted to empower you with practical skills
-            and knowledge, enhancing your readiness for the job market and
-            elevating your confidence in interviews.
-          </p>
-          <p className='sectionParagraph'>
-            Join us for a dynamic program where learning meets application, and
-            where the projects you work on aren&apos;t just exercises but
-            tangible demonstrations of your abilities. Don&apos;t miss this
-            opportunity to turbocharge your career with practical experience,
-            industry guidance, and a portfolio of impressive projects that truly
-            set you apart.
-          </p>
-        </div>
-      </section>
-      <section className='section bgBlue'>
-        <div className='sectionPadded'>
-          <h2>Cohort Information</h2>
-          <OfferingCard
-            text={cohortStatusMessage}
-            buttonText='Get Notified'
-            buttonLink='/'
-          />
-        </div>
-      </section>
-      <section className='section bgBlue'>
-        <div className='sectionPadded'>
-          <h2>Previous Cohorts</h2>
-          <div className={styles.yearSelector}>
-            {Object.keys(cohortData)
-              .sort((a, b) => Number(b) - Number(a))
-              .map((year) => (
-                <button
-                  key={year}
-                  className={`${styles.yearButton} ${
-                    selectedYear === Number(year) ? styles.active : ''
-                  }`}
-                  onClick={() => setSelectedYear(Number(year))}
-                >
-                  {year}
-                </button>
-              ))}
-          </div>
+      <Section isIntro classNames='bgBlue'>
+        <h1>Cohorts</h1>
+        <p>
+          Embark on a six-week journey with our completely free developer
+          placement program, designed to pair developers like you with industry
+          professionals for hands-on project experience. Guided by seasoned
+          developers, this program focuses on equipping you with real-world
+          skills and creating compelling projects to showcase in interviews.
+        </p>
+      </Section>
 
-          <div className={styles.cohortSection}>
-            <div className={styles.cohortList}>
-              {cohortData[selectedYear].reverse().map((cohort) => (
-                <CohortCard
-                  key={cohort.id}
-                  cohortName={cohort.cohortName}
-                  youtubeLink={cohort.youtubeLink}
-                />
-              ))}
-            </div>
+      <Section classNames='bgBlue'>
+        <h2>Cohort Information</h2>
+        <OfferingCard
+          text={cohortStatusMessage}
+          buttonText='Get Notified'
+          buttonLink='/'
+        />
+      </Section>
+
+      <Section classNames='bgBlue'>
+        <h2>Previous Cohorts</h2>
+        <div className={styles.yearSelector}>
+          {Object.keys(cohortData)
+            .sort((a, b) => Number(b) - Number(a))
+            .map((year) => (
+              <button
+                key={year}
+                className={`${styles.yearButton} ${
+                  selectedYear === Number(year) ? styles.active : ''
+                }`}
+                onClick={() => setSelectedYear(Number(year))}
+              >
+                {year}
+              </button>
+            ))}
+        </div>
+
+        <div className={styles.cohortSection}>
+          <div className={styles.cohortList}>
+            {cohortData[selectedYear].reverse().map((cohort) => (
+              <CohortCard
+                key={cohort.id}
+                cohortName={cohort.cohortName}
+                youtubeLink={cohort.youtubeLink}
+              />
+            ))}
           </div>
         </div>
-      </section>
+      </Section>
     </>
   );
 }
