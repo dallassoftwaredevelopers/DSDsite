44d44dbfdc9e6b9899d707f42d315532f805edb2
Dennis Garcia <BlueBoxDev@outlook.com>
2024-09-14 13:23:03 -0500

    (WEB-97) use video for intro and update css to fit

--- src/app/components/heroSection/heroSection.tsx
+++ src/app/components/heroSection/heroSection.tsx
@@ -10,7 +10,12 @@ export default function HeroSection() {
         <h1 className={styles.intro}>Level Up Your Coding with DSD</h1>
       </div>
       <div className={styles.purpleShape}>
-        <div className={styles.videoPlaceholder}>Video Playing Here</div>
+        <div className={styles.videoPlaceholder}>
+          <video width='320' loop autoPlay muted>
+            <source src='/assets/MeetupIntro.mp4' type='video/mp4' />
+            Your browser does not support the video tag.
+          </video>
+        </div>
       </div>
     </section>
   );
