138a65b082091fd0cd9d2e59632c5fd3422a34eb
Dennis Garcia <BlueBoxDev@outlook.com>
2024-12-29 19:22:27 -0600

    adjust appwrite to use api keys

--- lib/appwrite_client.ts
+++ lib/appwrite_client.ts
@@ -1,9 +1,10 @@
-import { Client } from 'appwrite';
+import * as sdk from 'node-appwrite';
 
-const client = new Client();
+const client = new sdk.Client();
 
 client
   .setEndpoint(process.env.APPWRITE_ENDPOINT as string)
-  .setProject(process.env.APPWRITE_PROJECT_ID as string);
+  .setProject(process.env.APPWRITE_PROJECT_ID as string)
+  .setKey(process.env.APPWRITE_SECRET as string);
 
 export default client;
