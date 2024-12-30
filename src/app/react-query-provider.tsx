e431d0fbdd41984818bf11925be51f77c1c4500b
Dennis Garcia <BlueBoxDev@outlook.com>
2024-12-29 19:23:44 -0600

    adjust default useQuery to prevent reload on window focus

--- src/app/react-query-provider.tsx
+++ src/app/react-query-provider.tsx
@@ -6,7 +6,11 @@ import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 export default function ReactQueryProvider({
   children,
 }: React.PropsWithChildren) {
-  const [client] = React.useState(new QueryClient());
+  const [client] = React.useState(
+    new QueryClient({
+      defaultOptions: { queries: { refetchOnWindowFocus: false } },
+    })
+  );
 
   return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 }
