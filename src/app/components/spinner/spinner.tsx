e2aae14eace3dcf04ead9833f85b9692d1e0912f
Eddie992 <ekataika2002@gmail.com>
2025-01-31 05:13:29 +0200

    chore: create a load spinner for data fetching (WEB-100) #115

--- src/app/components/spinner/spinner.tsx
+++ src/app/components/spinner/spinner.tsx
@@ -4,24 +4,7 @@ import styles from './spinner.module.css'; // Import css modules stylesheet as s
 export default function Spinner() {
   return (
     <div className={styles['spinner-container']}>
-      <div className={styles['loadingio-spinner-wedges-nq4q5u6dq7r']}>
-        <div className={styles['ldio-x2uulkbinbj']}>
-          <div>
-            <div>
-              <div></div>
-            </div>
-            <div>
-              <div></div>
-            </div>
-            <div>
-              <div></div>
-            </div>
-            <div>
-              <div></div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div className={styles['load-spinner']}></div>
     </div>
   );
 }
